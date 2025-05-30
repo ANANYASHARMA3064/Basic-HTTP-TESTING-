48d82e864ebf6536ab8cda31252bb1d86bb89f38
Ananya Sharma <ananyasharma3064@gmail.com>
2025-05-29 21:01:43 -0400

    added the about part for the odin project

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const http =require('http')
-const path =require('path')
-const fs=require('fs')
-const server =http.createServer((req,res)=>{
-    if (req.url==='/'){
-       fs.readFile(path.join(__dirname,'index.html'), (err,data)=>{
-        if (err){
-            res.WriteHead(500);//this gives status to the server 
-            res.end('Error handeling')//basically what I'll get on the screen when I run into an error
-        }
-        else{
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-       })
-
-    }
-    
-
-})
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error handeling');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/about') {
+    fs.readFile(path.join(__dirname, 'about.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error handeling');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  }
+});
 server.listen(8080)
